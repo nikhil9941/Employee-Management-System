95f0d8f7de6badd94e0a14f072b8a1f207d6b0e6
nikhil9941 <nikhiltarate03200@gmail.com>
2025-09-20 00:54:45 +0530

    employee viewsets

--- emp_backend/employee/views.py
+++ emp_backend/employee/views.py
@@ -2,3 +2,17 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Employee, Department, Attendance, LeaveRequest
 from .serializers import EmployeeSerializer, DepartmentSerializer, AttendanceSerializer, LeaveRequestSerializer
+
+class EmployeeViewSet(viewsets.ModelViewSet):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
+
+
+class DepartmentViewSet(viewsets.ModelViewSet):
+    queryset = Department.objects.all()
+    serializer_class = DepartmentSerializer
+
+class AttendanceViewSet(viewsets.ModelViewSet)::
+    queryset = Attendance.objects.all()
+    serializer_class = AttendanceSerializer
+    
